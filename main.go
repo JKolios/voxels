0a9e36beabc9b2bb734789342f15d60c39e94047
jkolios <jkolios@gophigital.com>
2017-12-02 13:59:32 +0200

    Skybox support, Rendering video to MJPEG files, refactors

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/andybons/gogif"
+	"github.com/icza/mjpeg"
 	"image"
-	"image/color"
 	"image/gif"
+	"image/jpeg"
 	"image/png"
 	"math"
 	"os"
 )
-
-func loadHeightMap(path string) heightMap {
-
-	img := loadImage(path)
-	heightMap := heightMap{}
-	for x := 0; x < img.Bounds().Max.X; x++ {
-		heightMapLine := []byte{}
-		for y := 0; y < img.Bounds().Max.Y; y++ {
-			heightMapByte := color.GrayModel.Convert(img.At(x, y)).(color.Gray).Y
-			heightMapLine = append(heightMapLine, heightMapByte)
-		}
-		heightMap = append(heightMap, heightMapLine)
-	}
-
-	return heightMap
-}
 
 func loadImage(path string) image.Image {
 
@@ -38,8 +24,8 @@ func loadImage(path string) image.Image {
 	}
 	defer imageFile.Close()
 
-	img, err := png.Decode(imageFile)
-
+	img, imgType, err := image.Decode(imageFile)
+	fmt.Printf("Image Type: %s", imgType)
 	if err != nil {
 		fmt.Printf("Failed to parse image, Error:%v", err.Error())
 		os.Exit(1)
@@ -53,7 +39,7 @@ func savePNG(path string, image image.Image) {
 	outfile, err := os.Create(path)
 	if err != nil {
 
-		fmt.Println("Cannot save output file, error: %v\n", err.Error())
+		fmt.Printf("Cannot save output file, error: %v\n", err.Error())
 		os.Exit(1)
 	}
 
@@ -66,15 +52,16 @@ func saveGIF(path string, images []image.Image, frameDelay int) {
 	outfile, err := os.Create(path)
 	if err != nil {
 
-		fmt.Println("Cannot save output file, error: %v\n", err.Error())
+		fmt.Printf("Cannot save output file, error: %v\n", err.Error())
 		os.Exit(1)
 	}
 
 	defer outfile.Close()
 
 	outGif := &gif.GIF{}
-	quantizer := gogif.MedianCutQuantizer{NumColor: 256}
-	for _, img := range images {
+	quantizer := gogif.MedianCutQuantizer{NumColor: 1024}
+	for count, img := range images {
+		fmt.Printf("Quantizing image %v\n", count)
 		bounds := img.Bounds()
 		palettedImage := image.NewPaletted(bounds, nil)
 		quantizer.Quantize(palettedImage, bounds, img, image.ZP)
@@ -84,6 +71,34 @@ func saveGIF(path string, images []image.Image, frameDelay int) {
 	gif.EncodeAll(outfile, outGif)
 }
 
+func saveMJPEG(path string, images []image.Image, fps int) {
+
+	frameSize := images[0].Bounds().Max
+	outFile, err := mjpeg.New(path, int32(frameSize.X), int32(frameSize.Y), int32(fps))
+	defer outFile.Close()
+	if err != nil {
+
+		fmt.Printf("Cannot create output file, error: %v\n", err.Error())
+		os.Exit(1)
+	}
+
+	for _, image := range images {
+
+		buf := &bytes.Buffer{}
+		if err := jpeg.Encode(buf, image, nil); err != nil {
+
+			fmt.Printf("Cannot encode image into JPEG, error: %v\n", err.Error())
+			os.Exit(1)
+		}
+
+		if err := outFile.AddFrame(buf.Bytes()); err != nil {
+
+			fmt.Printf("Cannot add frame to a JPEG file, error: %v\n", err.Error())
+			os.Exit(1)
+		}
+	}
+}
+
 func main() {
 
 	var viewPointX = flag.Float64("x", 0.0, "X coordinate of the render viewpoint")
@@ -91,34 +106,53 @@ func main() {
 	var viewPointZ = flag.Float64("z", 120.0, "Z coordinate of the render viewpoint")
 	var viewPointPhi = flag.Float64("phi", 0.0, "Phi angle of the render viewpoint")
 	var renderGif = flag.Bool("gif", false, "Flag for rendering a 360 degree GIF")
+	var renderPng = flag.Bool("png", true, "Flag for rendering a single PNG frame")
+	var renderMjpeg = flag.Bool("mjpeg", true, "Flag for rendering a 360 degree MJPEG")
 	flag.Parse()
 
 	fmt.Println("Voxels")
 	fmt.Println("Loading Heightmap...")
-	heightMap := loadHeightMap("height_map.png")
-	fmt.Printf("First heightmap byte: %v\n", heightMap[0][0])
+	heightMap := loadImage("height_map.png")
+	fmt.Printf("First heightmap byte: %v\n", heightMap.At(0, 0))
 	fmt.Println("Loading Colormap...")
 	colorMap := loadImage("color_map.png")
 	fmt.Printf("First colormap color: %+v\n", colorMap.At(0, 0))
+	fmt.Println("Loading Skybox...")
+	skyBox := loadImage("skybox.png")
 	fmt.Println("Initializing Renderer...")
-	options := RenderOptions{horizonHeight: 180.0, heightScale: 200.0, viewDistance: 200, screenWidth: 800, screenHeight: 600}
-	renderer := NewVoxelRenderer(heightMap, colorMap, &options)
+	options := RenderOptions{
+		horizonHeight:  180.0,
+		heightScale:    200.0,
+		renderDistance: 200,
+		renderBounds:   image.Rect(0, 0, 800, 600),
+	}
+	renderer := NewVoxelRenderer(heightMap, colorMap, skyBox, &options)
 
+	if *renderPng {
+		image := renderer.Render(*viewPointX, *viewPointY, *viewPointZ, *viewPointPhi)
+		savePNG("out.png", image)
+
+	}
 	if *renderGif {
 		images := []image.Image{}
-		coneImages := []image.Image{}
 
-		for angle := 0.0; angle < 2*math.Pi; angle += 0.1 {
-			newImage, newConeImage := renderer.Render(*viewPointX, *viewPointY, *viewPointZ, angle)
+		for angle := 0.0; angle < 2*math.Pi; angle += 0.2 {
+			newImage := renderer.Render(*viewPointX, *viewPointY, *viewPointZ, angle)
+			fmt.Printf("Render complete for angle: %v\n", angle)
 			images = append(images, newImage)
-			coneImages = append(coneImages, newConeImage)
 		}
 		saveGIF("out.gif", images, 10)
-		saveGIF("cones.gif", coneImages,10)
-	} else {
-		image, cone := renderer.Render(*viewPointX, *viewPointY, *viewPointZ, *viewPointPhi)
-		savePNG("out.png", image)
-		savePNG("cone.png", cone)
+	}
+	if *renderMjpeg {
+
+		images := []image.Image{}
+
+		for angle := 0.0; angle < 2*math.Pi; angle += 0.1 {
+			newImage := renderer.Render(*viewPointX, *viewPointY, *viewPointZ, angle)
+			fmt.Printf("Render complete for angle: %v\n", angle)
+			images = append(images, newImage)
+		}
+		saveMJPEG("out.mjpeg", images, 24)
 	}
 
 }
--- renderer.go
+++ renderer.go
@@ -12,66 +12,82 @@ var red color.RGBA = color.RGBA{255, 0, 0, 255}
 var green color.RGBA = color.RGBA{0, 255, 0, 255}
 var blue color.RGBA = color.RGBA{0, 0, 255, 255}
 
-type heightMap [][]byte
-
 type RenderOptions struct {
-	viewDistance, screenWidth, screenHeight int
-	horizonHeight, heightScale              float64
+	renderDistance             int
+	renderBounds               image.Rectangle
+	horizonHeight, heightScale float64
 }
 
 type VoxelRenderer struct {
-	hMap    heightMap
+	hMap    image.Image
 	cMap    image.Image
+	skyBox  image.Image
 	options *RenderOptions
 }
 
-func NewVoxelRenderer(hMap heightMap, cMap image.Image, options *RenderOptions) VoxelRenderer {
-	return VoxelRenderer{hMap: hMap, cMap: cMap, options: options}
+func NewVoxelRenderer(hMap, cMap, skyBox image.Image, options *RenderOptions) VoxelRenderer {
+
+	return VoxelRenderer{
+		hMap:    multiplyImage(hMap, 3, true),
+		cMap:    multiplyImage(cMap, 3, false),
+		skyBox:  skyBox,
+		options: options,
+	}
 }
 
-func (renderer VoxelRenderer) Render(x, y, height, phi float64) (image.Image, image.Image) {
-	renderBounds := image.Rect(0, 0, renderer.options.screenWidth, renderer.options.screenHeight)
-	renderImage := image.NewRGBA(renderBounds)
-
-	fillImage(renderImage, blue)
-
+func (renderer VoxelRenderer) Render(x, y, height, phi float64) image.Image {
 	sinphi := math.Sin(phi)
 	cosphi := math.Cos(phi)
 
-	coneBounds := renderer.cMap.Bounds()
-	cone := image.NewRGBA(coneBounds)
-	draw.Draw(cone, coneBounds, renderer.cMap, coneBounds.Min, draw.Src)
+	renderedFrame := image.NewRGBA(renderer.options.renderBounds)
+	draw.Draw(renderedFrame, renderer.options.renderBounds, renderer.skyBox, renderer.options.renderBounds.Min, draw.Src)
 
-	for z := renderer.options.viewDistance; z > 1; z-- {
+	floatScreenWidth := float64(renderer.options.renderBounds.Max.X)
+
+	for z := renderer.options.renderDistance; z > 1; z-- {
 		floatZ := float64(z)
-		floatScreenWidth := float64(renderer.options.screenWidth)
 
-		leftX := math.Abs(math.Mod((-cosphi*floatZ - sinphi*floatZ) + x,  float64(renderer.cMap.Bounds().Max.X)))
-		leftY := math.Abs(math.Mod((sinphi*floatZ - cosphi*floatZ) + y,  float64(renderer.cMap.Bounds().Max.Y)))
-		rightX := math.Abs(math.Mod((cosphi*floatZ - sinphi*floatZ) + x,  float64(renderer.cMap.Bounds().Max.X)))
-		rightY := math.Abs(math.Mod((-sinphi*floatZ - cosphi*floatZ) + y,  float64(renderer.cMap.Bounds().Max.Y)))
+		leftX := (-cosphi*floatZ - sinphi*floatZ) + x
+		leftY := (sinphi*floatZ - cosphi*floatZ) + y
+		rightX := (cosphi*floatZ - sinphi*floatZ) + x
+		rightY := (-sinphi*floatZ - cosphi*floatZ) + y
 
 		dx := (rightX - leftX) / floatScreenWidth
 		dy := (rightY - leftY) / floatScreenWidth
-		for i := 0; i < renderer.options.screenWidth; i++ {
+		for i := 0; i < renderer.options.renderBounds.Max.X; i++ {
 
-			cone.Set(int(leftX), int(leftY), blue)
-			heightOnScreen := float64(byte(height)-renderer.hMap[int(leftX)][int(leftY)])/floatZ*renderer.options.heightScale + renderer.options.horizonHeight
-			drawVerticalLineFromPoint(renderImage, image.Pt(i, int(heightOnScreen)), renderer.cMap.At(int(leftX), int(leftY)))
+			mapHeight := renderer.hMap.At(int(leftX), int(leftY)).(color.Gray).Y
+			heightOnScreen := float64(uint8(height)-mapHeight)/floatZ*renderer.options.heightScale + renderer.options.horizonHeight
+			drawVerticalLineFromPoint(renderedFrame, image.Pt(i, int(heightOnScreen)), renderer.cMap.At(int(leftX), int(leftY)))
 			leftX += dx
 			leftY += dy
 		}
 	}
 
-	return renderImage, cone
+	return renderedFrame
 }
 
-func drawVerticalLineFromPoint(img *image.RGBA, startPoint image.Point, color color.Color) {
+func drawVerticalLineFromPoint(img draw.Image, startPoint image.Point, color color.Color) {
 
 	drawRectangle := image.Rect(startPoint.X, startPoint.Y, startPoint.X+1, img.Bounds().Max.Y)
 	draw.Draw(img, drawRectangle, &image.Uniform{color}, image.ZP, draw.Src)
 }
 
-func fillImage(img *image.RGBA, color color.Color) {
-	draw.Draw(img, img.Bounds(), &image.Uniform{color}, image.ZP, draw.Src)
+func multiplyImage(img image.Image, factor int, grayscale bool) image.Image {
+
+	outputBounds := image.Rect(0, 0, img.Bounds().Max.X*factor, img.Bounds().Max.Y*factor)
+	var outputImage draw.Image
+	if grayscale {
+		outputImage = image.NewGray(outputBounds)
+	} else {
+		outputImage = image.NewRGBA(outputBounds)
+	}
+	for i := 0; i < factor; i++ {
+		for j := 0; j < factor; j++ {
+			startPoint := image.Pt(j*img.Bounds().Max.X, i*img.Bounds().Max.Y)
+			copyBounds := image.Rectangle{startPoint, startPoint.Add(img.Bounds().Size())}
+			draw.Draw(outputImage, copyBounds, img, image.ZP, draw.Src)
+		}
+	}
+	return outputImage
 }
